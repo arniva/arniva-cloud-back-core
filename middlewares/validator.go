3abde63d0041d663bd0a12b11a48b5ce9c6181ff
ismailozdel <ismailozdel@gmail.com>
2025-05-29 15:21:40 +0300

    validasyon mesajlari, array insertlerdeki validasyon problemi fix

--- middlewares/validator.go
+++ middlewares/validator.go
@@ -2,10 +2,14 @@ package middlewares
 
 import (
 	"fmt"
+	"reflect"
 	"strings"
 
 	"github.com/arniva/arniva-cloud-back-core/httputils"
+	"github.com/go-playground/locales/tr"
+	ut "github.com/go-playground/universal-translator"
 	"github.com/go-playground/validator/v10"
+	trTranslations "github.com/go-playground/validator/v10/translations/tr"
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -18,11 +22,26 @@ type ErrorResponse struct {
 	FailedField string
 	Tag         string
 	Value       interface{}
+	Str         string
 }
 
 var Validator = &XValidator{Validator: validator.New()}
+var turkish = tr.New()
+var uni = ut.New(turkish, turkish)
+var translator, f = uni.GetTranslator("tr")
+
+func init() {
+
+	if err := trTranslations.RegisterDefaultTranslations(Validator.Validator, translator); err != nil {
+		panic(fmt.Sprintf("Failed to register translations: %v", err))
+	}
+
+}
 
 func (x *XValidator) Validate(data interface{}, exclude *[]string) []ErrorResponse {
+	// Test: çeviri kayıtlı mı?
+	// fmt.Println(translator.T("required", "StokID")) // "StokID is a required field"
+
 	validationErrors := []ErrorResponse{}
 
 	excludeMap := make(map[string]interface{})
@@ -30,23 +49,51 @@ func (x *XValidator) Validate(data interface{}, exclude *[]string) []ErrorRespon
 		excludeMap[strings.ToLower(v)] = nil
 	}
 
-	errs := x.Validator.Struct(data)
-	if errs != nil {
-		for _, err := range errs.(validator.ValidationErrors) {
-			// In this case data object is actually holding the User struct
-			var elem ErrorResponse
+	val := reflect.ValueOf(data)
+	kind := val.Kind()
+	if kind == reflect.Ptr {
+		val = val.Elem()
+		kind = val.Kind()
+	}
 
-			elem.FailedField = err.Field() // Export struct field name
-			elem.Tag = err.Tag()           // Export struct tag
-			elem.Value = err.Value()       // Export field value
-			elem.Error = true
+	if kind == reflect.Slice || kind == reflect.Array {
+		for i := 0; i < val.Len(); i++ {
+			item := val.Index(i).Interface()
+			errs := x.Validator.Struct(item)
+			if errs != nil {
+				for _, err := range errs.(validator.ValidationErrors) {
+					var elem ErrorResponse
+					elem.FailedField = err.Field()
+					elem.Tag = err.Tag()
+					elem.Value = err.Value()
+					elem.Str = err.Translate(translator)
+					elem.Error = true
+					if _, exist := excludeMap[elem.FailedField]; !exist {
+						validationErrors = append(validationErrors, elem)
+					}
+				}
+			}
+		}
+	} else {
+		errs := x.Validator.Struct(data)
 
-			if _, exist := excludeMap[elem.FailedField]; !exist {
-				validationErrors = append(validationErrors, elem)
+		if errs != nil {
+			for _, err := range errs.(validator.ValidationErrors) {
+				// In this case data object is actually holding the User struct
+				var elem ErrorResponse
+
+				elem.FailedField = err.Field()       // Export struct field name
+				elem.Tag = err.Tag()                 // Export struct tag
+				elem.Value = err.Value()             // Export field value
+				elem.Str = err.Translate(translator) // Export translated error message
+				elem.Error = true
+
+				if _, exist := excludeMap[elem.FailedField]; !exist {
+					validationErrors = append(validationErrors, elem)
+				}
 			}
 		}
 	}
-
 	return validationErrors
 }
 
@@ -54,14 +101,10 @@ func Validate[T any](exclude []string) fiber.Handler {
 	return func(c *fiber.Ctx) error {
 		data := c.Locals("data").(*T)
 		if errs := Validator.Validate(data, &exclude); len(errs) > 0 && errs[0].Error {
+
 			errMsgs := make([]string, len(errs))
 			for i, err := range errs {
-				errMsgs[i] = fmt.Sprintf(
-					"[%s]: '%v' | Needs to implement '%s'",
-					err.FailedField,
-					err.Value,
-					err.Tag,
-				)
+				errMsgs[i] = err.Str
 			}
 			return httputils.NewApiError(400, httputils.Enums.Code2, strings.Join(errMsgs, " and "))
 		}
